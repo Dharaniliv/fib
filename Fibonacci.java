255083cb934084c24fddc93443f6afe167ae1783
Dharaniliv <dharaniprasath8015@gmail.com>
2025-03-14 11:00:43 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 10; // Number of terms
+        int n = 10; 
         int first = 0, second = 1;
         System.out.print("Fibonacci Series: " + first + ", " + second);
 
